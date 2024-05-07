3828bcf9378dcecdf89f97e6c8613589e0add297
Chani-Rotenberg <166316112+Chani-Rotenberg@users.noreply.github.com>
2024-05-07 14:15:24 +0300

    add file: arr & server

---
+++ arr.js
@@ -0,0 +1,18 @@
+let arr = [
+    {id:1, name:"avi", count:4}, 
+    {id:2, name:"yohai", count:18}, 
+    {id:3, name:"moshe", count:5}, 
+    {id:4, name:"meir", count:1}, 
+  ]
+   const remove=(name) =>{
+    arr=arr.filter(person=>person.name!=name)
+    return arr}
+  
+   const add=(item) =>{arr.push(item)
+    return arr}
+  
+   const update=(name)=>{
+    arr=arr.map(person=>{if(person.name==name){person.count++};return person})
+    return arr}
+   
+    module.exports={remove,add,update}
---
+++ server.js
@@ -0,0 +1,5 @@
+const {remove, add, update} = require('./arr')
+let p={id:8, name:"aaa", count:12}
+//console.log(remove("moshe"))
+//console.log(add(p))
+console.log(update("yohai"))
